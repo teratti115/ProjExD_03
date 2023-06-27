cc88c8020fa2c52f7809b1d28050f5773beb74f0
teratti <c0b22106d3@edu.teu.ac.jp>
2023-06-27 15:58:19 +0900

    なんかコミット

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -7,6 +7,7 @@ import pygame as pg
 
 WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
+NUM_OF_BOMBS = 5  # 爆弾の数
 
 
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
@@ -144,7 +145,8 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("ex03/fig/pg_bg.jpg")
     bird = Bird(3, (900, 400))
-    bomb = Bomb((255, 0, 0), 10)
+    # bomb = Bomb((255, 0, 0), 10)
+    bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     beam = None
 
     clock = pg.time.Clock()
@@ -157,25 +159,28 @@ def main():
                 beam = Beam(bird)  # ビームクラスのインスタンスを生成する
                 
         screen.blit(bg_img, [0, 0])
-        
-        if bomb is not None:
+        if bomb  is not None:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
                 return
-        
         if beam is not None and bomb is not None:
             if bomb.rct.colliderect(beam.rct):
                 bomb = None
                 beam = None
                 bird.change_img(6, screen)
-                pg.display.update()              
+                pg.display.update()
+                time.sleep(1)
+
+
+
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        if bomb is not None:
+        bombs = [bomb for bomb in bombs if bomb is not None]
+        for bomb in bombs:
             bomb.update(screen)
         if beam is not None:
             beam.update(screen)
@@ -189,3 +194,4 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+ 
